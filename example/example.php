c9b9ce22aefc5940ee0588afbe18eb3e62464f08
guidoexge <guido.valle@gmail.com>
2024-03-05 12:05:15 +0100

    Update example.php

--- example/example.php
+++ example/example.php
@@ -2,7 +2,7 @@
 
 require_once '\htdocs\wwwatb\vendor\autoload.php';
 
-$pdf = new Barcode_FPDF ('L','mm','A4');
+$pdf = new Barcode_FPDF ('L','mm','A5');
 
 	$pdf->AddPage();
 	$pdf->SetAutoPageBreak(0,1);
@@ -17,13 +17,16 @@ $pdf = new Barcode_FPDF ('L','mm','A4');
 	$height = 5.0 ;
 	$fontData = array('Arial','',4);
  	$pdf->code39($x, $y,$codice_barre,$width,$height,false,false,false,true, $fontData , "R", "L");
+	$x = 45;
+	$y = 18;	
+ 	$pdf->code39($x, $y,$codice_barre,$width,$height,false,false,false,true, $fontData , "R", "P");
 
 	$pdf->SetFont('Arial', '', 5);
 	$pdf->SetXY(8,24);
 
 
 //	$pdf->Cell(0,0,$codice_barre,0,2,'C');
- 	$pdf->TextWithRotation(40,15,'Made in ITALY','U',0);
+ 	$pdf->TextWithRotation(35,15,'Made in ITALY','U',0);
   	$pdf->TextWithRotation(60,25,'Made in ITALY',75,25);
  	$pdf->TextWithRotation(80,45,'Made in ITALY','D',0);
 	$pdf->Output($filename,'F', true);
